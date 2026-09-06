028a522aa0ce4d3858d51d2f92beb3506ad6010f
Drumcito <brandon.asc15@outlook.com>
2026-09-06 17:25:09 -0600

    Cambios de nombre (estandar ES)

--- app/controllers/AuthController.php
+++ app/controllers/AuthController.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-require_once __DIR__ . '/../../config/connectionDB.php';
+require_once __DIR__ . '/../../config/conexionBD.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $numEmpleado = trim($_POST['numero_empleado'] ?? '');
--- crear_usuarios.php
+++ crear_usuarios.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/config/connectionDB.php';
+require_once __DIR__ . '/config/conexionBD.php';
 
 try {
     $pdo = Database::getConnection();
