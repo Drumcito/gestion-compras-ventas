0810ae2c12a05e2215348ca2f607b3d6b878f788
Drumcito <brandon.asc15@outlook.com>
2026-09-07 12:30:35 -0600

    Conexion BD

---
+++ config/credenciales.local.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Credenciales de MI maquina. No se versiona (.gitignore).
+ * Produccion NO usa este archivo: alla van variables de entorno.
+ */
+
+return [
+    'host'    => 'localhost',
+    'db'      => 'gabe_system',
+    'user'    => 'root',
+    'pass'    => '',
+    'charset' => 'utf8mb4',
+];
